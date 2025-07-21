30e44ae820bf94a6e345ae28eb7d7fc65f2448e8
Birhanu <birhanumatebe12@gmail.com>
2025-07-21 21:02:12 +0300

    feat: implement ThemeContext

---
+++ src/contexts/ThemeContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState("dark");
+
+  useEffect(() => {
+    const html = document.documentElement;
+    html.classList.remove("light", "dark");
+    html.classList.add(theme);
+  }, [theme]);
+
+  const toggleTheme = () =>
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("state is used outside from the provided component");
+  }
+  return context;
+}
