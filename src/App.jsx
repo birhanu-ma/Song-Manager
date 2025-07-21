2b675cce9bc421d9e59cdd125e75b4f2de75cc44
Birhanu <birhanumatebe12@gmail.com>
2025-07-21 20:44:08 +0300

    feat: implement initial route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+import SongDetails from "./features/song/SongDetails";
+import EditeSong from "./features/song/EditSong";
+import SongForm from "./features/song/SongForm";
+
+createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      { path: "/songs/:id", element: <SongDetails /> },
+      { path: "/newsong", element: <SongForm /> },
+      { path: "/edit/:id", element: <EditeSong /> },
+    ],
+  },
+]);
 function App() {
-  return (
-    <div>
-      <h1>hello birhe from Addis</h1>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
