f53d1bb9208af9593e29ab3fb55f1ec9667f14d2
Birhanu <birhanumatebe12@gmail.com>
2025-07-21 21:30:05 +0300

    feat: implement SongItem to render songs

---
+++ src/ui/SongItem.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { deleteSong } from "../features/song/SongsSlice";
+import { useDispatch } from "react-redux";
+function SongItem({ song }) {
+  const dispatch = useDispatch();
+  if (!song) return null;
+  const { id, title, artist, album, date } = song;
+
+  function handleDelete(e) {
+    e.preventDefault();
+    console.log("delete get clicked");
+    dispatch(deleteSong(id));
+  }
+  return (
+    <div>
+      <li>
+        <Link className="link" to={`/songs/${id}`}>
+          <span>{title}</span>
+          <span>{artist}</span>
+          <span>{album}</span>
+          <span>{date}</span>
+        </Link>
+        <button onClick={handleDelete} className="delete">
+          &times;
+        </button>
+      </li>
+    </div>
+  );
+}
+
+export default SongItem;
