783d2cf03804947999d53650cdaa5099e4f3d655
Birhanu <birhanumatebe12@gmail.com>
2025-07-21 16:30:55 +0300

    webpack set up

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <div>
+      <h1>hello birhe from Addis</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.bundle.js",
+  },
+  module: {
+    rules: [
+      { test: /\.jsx?$/, exclude: /node_modules/, use: "babel-loader" },
+      { test: /\.(png|jpe?g|gif|svg)$/i, use: "asset/resource" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+    new Dotenv(),
+  ],
+  devServer: {
+    static: "./dist",
+    port: 10000,
+    open: true,
+    hot: true,
+  },
+};
