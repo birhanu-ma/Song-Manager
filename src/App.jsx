159fc6906d9f9b9ff0369234590487824b45a308
Birhanu <birhanumatebe12@gmail.com>
2025-07-22 15:01:23 +0300

    refactor: route configure correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import React, { lazy, Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Lazy-loaded components
 const AppLayout = lazy(() => import("./pages/AppLayout.jsx"));
+const HomePage = lazy(() => import("./pages/HomePage.jsx"));
 const SongDetails = lazy(() => import("./features/song/SongDetails.jsx"));
-const EditeSong = lazy(() => import("./features/song/EditSong.jsx"));
+const EditSong = lazy(() => import("./features/song/EditSong.jsx"));
 const SongForm = lazy(() => import("./features/song/SongForm.jsx"));
+const NotFound = lazy(() => import("./pages/PageNotFound.jsx"));
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/songs",
     element: <AppLayout />,
     children: [
-      { path: "/songs/:id", element: <SongDetails /> },
-      { path: "/newsong", element: <SongForm /> },
-      { path: "/edit/:id", element: <EditeSong /> },
+      { path: ":id", element: <SongDetails /> },
+      { path: "edit/:id", element: <EditSong /> },
+      { path: "newsong", element: <SongForm /> },
     ],
   },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
 ]);
+
 function App() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
@@ -23,4 +36,5 @@ function App() {
     </Suspense>
   );
 }
+
 export default App;
