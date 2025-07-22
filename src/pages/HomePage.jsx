27fe7fc3653c268fb243066a407b7fe128b6baab
Birhanu <birhanumatebe12@gmail.com>
2025-07-22 15:02:40 +0300

    feat: add HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function HomePage() {
+  const navigate = useNavigate();
+  function handleStarted() {
+    navigate("/songs");
+  }
+
+  return (
+    <div className="homepage">
+      <h1>🎵 Song Manager</h1>
+      <p>
+        Welcome to the Song Manager app a simple yet powerful tool to manage
+        your music collection.
+      </p>
+      <button className="btn" onClick={handleStarted}>
+        get started
+      </button>
+    </div>
+  );
+}
+
+export default HomePage;
